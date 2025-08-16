4c735c795f5eca8148fd6a4211450fd8d2418e5b
soozhin <53286620+soozhin@users.noreply.github.com>
2025-08-16 16:38:31 +0900

    refactor: Use a base download class for video, audio and transcript download classese for a cleaner implementation

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -1,8 +1,31 @@
 # These paths are used to store downloaded videos and audios.
-DOWNLOADED_VIDEO_PATH = "./backend/download/downloaded_videos"
-DOWNLOADED_AUDIO_PATH = "./backend/download/downloaded_audios"
-
+DOWNLOADED_VIDEO_DIR = "./backend/download/downloaded_videos"
+DOWNLOADED_AUDIO_DIR = "./backend/download/downloaded_audios"
+DOWNLOADED_TRANSCRIPT_DIR = "./backend/download/downloaded_transcripts"
+DOWNLOADED_VIDEO_PATH = f"{DOWNLOADED_VIDEO_DIR}/%(title)s.%(ext)s"
+DOWNLOADED_AUDIO_PATH = f"{DOWNLOADED_AUDIO_DIR}/%(title)s.%(ext)s"
+DOWNLOADED_TRANSCRIPT_PATH = f"{DOWNLOADED_TRANSCRIPT_DIR}/%(title)s.%(ext)s"
 
 # Constants for video and audio formats
 VIDEO_FORMAT = 'bestvideo+bestaudio/best/mp4'
-AUDIO_FORMAT = 'bestaudio/mp3'
+AUDIO_FORMAT = 'mp3/bestaudio'
+LANGUAGE = 'ja'  # Default language for transcription
+
+VIDEO_OPTION = {
+    'format': VIDEO_FORMAT,
+    'outtmpl': DOWNLOADED_VIDEO_PATH,
+}
+
+AUDIO_OPTION = {
+    'format': AUDIO_FORMAT,
+    'outtmpl': DOWNLOADED_AUDIO_PATH,
+}
+
+TRANSCRIPT_OPTION = {
+    'writesubtitles': True,
+    'subtitleslangs': ['ja'],  # Japanese subtitles
+    'outtmpl': DOWNLOADED_TRANSCRIPT_PATH,
+    'skip_download': True,  # Skip downloading the video
+    'writeautomaticsub': True,  # Download automatic subtitles if available
+    'subtitlesformat': 'vtt',  # Use VTT format for subtitles
+}
