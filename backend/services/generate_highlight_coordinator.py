c8493832bafcc8067d12aa63a33714f6f0cd4ca1
soozhin <53286620+soozhin@users.noreply.github.com>
2025-08-30 16:20:26 +0900

    feat: Add a coordinator that coordinates all backend services

---
+++ backend/services/generate_highlight_coordinator.py
@@ -0,0 +1,60 @@
+from backend.services.youtube_download import TranscriptDownloadService, VideoDownloadService
+from backend.services.transcript_parser import TranscriptParser
+from backend.services.highlight_detection import HighlightDetectionService
+from backend.services.video_clipping import VideoClippingService
+
+from typing import List
+from concurrent.futures import ThreadPoolExecutor
+
+
+class GenerateHighlightCoordinator:
+    """This class coordinates the generation of highlights in the backend.
+    It manages the interaction between different services
+    such as video, audio, and transcript download services.
+    """
+
+    def run(self, video_url: str) -> List:
+        """
+        This method coordinates all the services in the backend.
+        """
+
+        # Download video and transcript
+        video_service = VideoDownloadService()
+        transcript_service = TranscriptDownloadService()
+
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            # Download video on a different thread
+            video_future = executor.submit(video_service.download, video_url)
+
+            transcript_path = transcript_service.download(video_url)
+            print(f"Transcript downloaded to: {transcript_path}")
+
+            # Parse the transcript
+            parser = TranscriptParser()
+            parsed_entries = parser.parse(transcript_path)
+            print(f"Parsed Transcript Entries count: {len(parsed_entries)}")
+
+            # Detect highlights from the parsed transcript
+            highlight_service = HighlightDetectionService()
+            highlights = highlight_service.execute(parsed_entries)
+            print(f"Detected Highlights count: {len(highlights)}")
+
+            # Clip the video based on detected highlights
+            video_path = video_future.result()
+            # video_path = r"/Users/limsoozhin/Documents/Program/Vtuber-highlight-generator/backend/download/downloaded_videos/【Minecraft】視聴者１００人サーバーを観光してみる！！！！！　#結城さくな生放送.mp4"
+            video_clipping_service = VideoClippingService(video_path)
+            clipped_videos = video_clipping_service.clip(highlights)
+            print(f"Clipped Videos: {clipped_videos}")
+
+        return clipped_videos
+
+
+# Example usage
+if __name__ == "__main__":
+    highlight_generator = GenerateHighlightCoordinator()
+    video_url = "https://www.youtube.com/watch?v=UcE0Go6I0XI"
+    clipped_videos = highlight_generator.run(video_url)
+
+    print("Clipped Videos:")
+    for video in clipped_videos:
+        print(video)
