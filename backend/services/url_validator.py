fac09f358f9efcdd6aaea84ee41cc2bfb85e74be
soozhin <53286620+soozhin@users.noreply.github.com>
2025-09-07 15:34:06 +0900

    feat: Validate a provided youtube url

---
+++ backend/services/url_validator.py
@@ -0,0 +1,20 @@
+from backend.core.constants import YOUTUBE_REGEX
+
+
+class UrlValidator:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def is_validate_url(url: str):
+        is_validate_url = bool(YOUTUBE_REGEX.match(url))
+        return is_validate_url
+
+
+# Example usage
+if __name__ == "__main__":
+    url1 = "random_str1"
+    url2 = "https://www.youtube.com/watch?v=UcE0Go6I0XI"
+
+    print(f"Url1 validity: {UrlValidator.is_validate_url(url1)}")
+    print(f"Url2 validity: {UrlValidator.is_validate_url(url2)}")
