84411de988b2f7bdcb39ea803d437117402f42f7
soozhin <53286620+soozhin@users.noreply.github.com>
2025-09-23 13:38:01 +0900

    fix: Added origins to allow connection from frontend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from backend.routers.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -9,6 +10,17 @@ app = FastAPI(
 
 app.include_router(router, prefix="/api/v1")
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # For demonstration purposes
 if __name__ == "__main__":
